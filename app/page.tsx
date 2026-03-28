025d2855e90bdac35d7bb6ddd8fc24ed1de651f6
Sravan Surya <vsravansurya@gmail.com>
2026-03-28 23:56:50 +0530

    feat: add Footer and Skills components, update Data with skills and social links

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Blogs from "./Blogs/page";
 import Hero from "./Hero/page";
 import Projects from "./Projects/page";
 import Socialmedia from "./SocailMedia/page";
+import Skills from "./Skills/page";
+import Footer from "./Footer/page";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <Socialmedia />
       <Projects />
       <Blogs />
+      <Skills />
+      <Footer />
     </div>
   );
 }
--- components/Data.tsx
+++ components/Data.tsx
@@ -1,6 +1,36 @@
-import { SiGithub, SiLeetcode } from "react-icons/si";
+import {
+  SiGithub,
+  SiLeetcode,
+  SiTypescript,
+  SiReact,
+  SiNodedotjs,
+  SiExpress,
+  SiMongodb,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiGit,
+  SiDocker,
+  SiPython,
+  SiFigma,
+  SiPrisma,
+  SiRedis,
+  SiRedux,
+  SiGreensock,
+  SiLinux,
+  SiReactquery,
+  SiShadcnui,
+  SiDrizzle,
+} from "react-icons/si";
 import { BsTwitterX } from "react-icons/bs";
-import { FaLinkedinIn, FaMedium } from "react-icons/fa6";
+import {
+  FaLinkedinIn,
+  FaMedium,
+  FaJava,
+  FaAws,
+  FaDatabase,
+  FaInstagram,
+} from "react-icons/fa6";
+import { RiJavascriptFill } from "react-icons/ri";
 
 export const socialLinks = [
   {
@@ -27,5 +57,138 @@ export const socialLinks = [
     name: "LeetCode",
     url: "https://leetcode.com/u/vsravansurya/",
     icon: <SiLeetcode />,
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/sravansurya_28?igsh=Z3JnZHZhaWp4cGZ3",
+    icon: <FaInstagram />,
+  },
+];
+
+export const skills = [
+  {
+    name: "JavaScript",
+    icon: <RiJavascriptFill />,
+    url: "https://www.google.com/search?q=javascript&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript />,
+    url: "https://www.google.com/search?q=typescript&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "React",
+    icon: <SiReact />,
+    url: "https://www.google.com/search?q=react&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Node.js",
+    icon: <SiNodedotjs />,
+    url: "https://www.google.com/search?q=node.js&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Express.js",
+    icon: <SiExpress />,
+    url: "https://www.google.com/search?q=express.js&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "MongoDB",
+    icon: <SiMongodb />,
+    url: "https://www.google.com/search?q=mongodb&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Next.js",
+    icon: <SiNextdotjs />,
+    url: "https://www.google.com/search?q=next.js&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: <SiTailwindcss />,
+    url: "https://www.google.com/search?q=tailwind+css&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Git",
+    icon: <SiGit />,
+    url: "https://www.google.com/search?q=git&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Docker",
+    icon: <SiDocker />,
+    url: "https://www.google.com/search?q=docker&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Java",
+    icon: <FaJava />,
+    url: "https://www.google.com/search?q=java&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Python",
+    icon: <SiPython />,
+    url: "https://www.google.com/search?q=python&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Github",
+    icon: <SiGithub />,
+    url: "https://www.google.com/search?q=github&sxsrf=ANbL-n7bNXvRiH6VslHsiuCOFqp0F7sSbQ%3A1774713607405",
+  },
+  {
+    name: "Figma",
+    icon: <SiFigma />,
+    url: "https://www.google.com/search?q=figma&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Tanstack Query",
+    icon: <SiReactquery />,
+    url: "https://www.google.com/search?q=tanstack+query&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Prisma",
+    icon: <SiPrisma />,
+    url: "https://www.google.com/search?q=prisma&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Redis",
+    icon: <SiRedis />,
+    url: "https://www.google.com/search?q=redis&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Redux",
+    icon: <SiRedux />,
+    url: "https://www.google.com/search?q=redux&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "SQL",
+    icon: <FaDatabase />,
+    url: "https://www.google.com/search?q=sql&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "ShadCN",
+    icon: <SiShadcnui />,
+    url: "https://www.google.com/search?q=shadcn&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Gsap",
+    icon: <SiGreensock />,
+    url: "https://www.google.com/search?q=gsap&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Zustand",
+    icon: <FaDatabase />,
+    url: "https://www.google.com/search?q=zustand&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  }, // using db icon as fallback
+  {
+    name: "Linux",
+    icon: <SiLinux />,
+    url: "https://www.google.com/search?q=linux&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Drizzle",
+    icon: <SiDrizzle />,
+    url: "https://www.google.com/search?q=drizzle&sxsrf=ANbL-n七bNXvRiH6VslHsiuCOFqp0F七sSbQ%3A1七年4七1360七年405",
+  },
+  {
+    name: "Aws",
+    icon: <FaAws />,
+    url: "https://www.google.com/search?q=aws&sxsrf=ANbL-n seven bNXvRiH6VslHsiuCOFqp0F seven sSbQ%3A1 seven 4 seven 1360 seven 405",
   },
 ];
