36fb606773977d5ff7c3448afb82adb7270687fc
Sravan Surya <vsravansurya@gmail.com>
2026-04-11 12:57:29 +0530

    fix: standardize class names for consistency in AboutMe component

--- app/AboutMe/page.tsx
+++ app/AboutMe/page.tsx
@@ -19,7 +19,7 @@ const AboutMe = () => {
     <div className="px-5 py-3  mt-5 rounded-lg ">
       <p className="font-normal">
         Hey, I'm Sravan Surya a{" "}
-        <span className="font-bold text-[var(--primary-foreground)]">
+        <span className="font-bold text-primary-foreground">
           full-stack developer
         </span>{" "}
         who enjoys building modern web apps where clean design, performance, and
@@ -27,33 +27,33 @@ const AboutMe = () => {
       </p>
       <p className="font-normal">
         I mostly work with{" "}
-        <span className="text-[var(--primary-foreground)] font-bold">
+        <span className="text-primary-foreground font-bold">
           JavaScript,TypeScript, React, Next.js, Node.js, PostgreSQL and MongoDB
         </span>
         , and I'm always open to learning new technologies and growing through
         new opportunities.
       </p>
 
-      <div className="flex items-center flex-wrap justify-start mt-3 gap-3">
+      <div className="mt-3 flex flex-nowrap items-center justify-start gap-2 overflow-x-auto pb-1 sm:flex-wrap sm:overflow-visible sm:gap-3">
         <Button
-          className="cursor-pointer"
+          className="cursor-pointer shrink-0"
           onClick={() => openLink("https://x.com/SS19732004")}
         >
           <SiMinutemailer /> Twitter Dm
         </Button>
-        <div className="text-bold text-sm -rotate-45">OR</div>
+        <div className="shrink-0 text-bold text-sm -rotate-45">OR</div>
         <Button
-          className="cursor-pointer"
+          className="cursor-pointer shrink-0"
           onClick={() => openLink("mailto:vsravansurya@gmail.com")}
         >
           <IoMail /> Email Me
         </Button>
-        <div>|</div>
+        <div className="shrink-0">|</div>
 
         <Tooltip>
           <TooltipTrigger asChild>
             <Button
-              className="cursor-pointer"
+              className="cursor-pointer shrink-0"
               onClick={() => openLink("https://github.com/Sravan2808")}
             >
               <SiGithub />
@@ -67,7 +67,7 @@ const AboutMe = () => {
         <Tooltip>
           <TooltipTrigger asChild>
             <Button
-              className="cursor-pointer"
+              className="cursor-pointer shrink-0"
               onClick={() =>
                 openLink(
                   "https://drive.google.com/file/d/1SD95XKcO0feTLfJ77l5OIEn1Ihz0oIYe/view?usp=sharing",
